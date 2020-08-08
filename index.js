276bc7c90f59a1325fe8b7ee70cf46fed5eb97dd
peawarrior <jacksonchen171@gmail.com>
2020-08-08 19:49:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(array, fn) {
+    let ans = []
+    for (let i = 0; i < array.length; i++) {
+        ans.push(fn(array[i]))
+    }
+    return ans
+}
+
+function reduce(array, fn, start = 0) {
+    let ans = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        ans = fn(array[i], ans)
+    }
+    return ans
+}
