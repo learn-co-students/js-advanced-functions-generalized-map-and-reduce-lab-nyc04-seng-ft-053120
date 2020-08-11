8bb81e09d99430481161475a6b2717a670473803
vuonga1103 <vuonga1103@yahoo.com>
2020-08-11 16:14:46 -0400

    almost finished

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+const map = (sourceArray, callback) => {
+  const result = [];
+  sourceArray.forEach(el => result.push(callback(el)));
+  return result;
+}
+
+const reduce = (sourceArray, callback, startingValue = 0) => {
+  let total = startingValue;
+  sourceArray.forEach(el => (total = callback(total,el)));
+  return total;
+}
