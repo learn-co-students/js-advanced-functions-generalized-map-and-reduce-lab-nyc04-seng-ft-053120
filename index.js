1c9a5d31e5ec4566c5e1ee7f176780d3ce6b1664
“DonatelloTHM” <donat.pllana@outlook.com>
2020-08-12 00:33:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+const map = (sourceArray, callback) => {
+    const result = [];
+    sourceArray.forEach((el) => result.push(callback(el)));
+    return result;
+  };
+  
+  const reduce = (sourceArray, callback, startingValue = 0) => {
+    let total = startingValue;
+  
+    for (let i = 0; i < sourceArray.length; i++) {
+      if (total === 0) {
+        total = sourceArray[0];
+        i = 1;
+      }
+  
+      total = callback(total, sourceArray[i]);
+    }
+    return total;
+  };
