4bf28c8cb589f30d0e90b000f5d3c209eab9f94f
vuonga1103 <vuonga1103@yahoo.com>
2020-08-11 21:29:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const map = (sourceArray, callback) => {
   const result = [];
-  sourceArray.forEach(el => result.push(callback(el)));
+  sourceArray.forEach((el) => result.push(callback(el)));
   return result;
-}
+};
 
 const reduce = (sourceArray, callback, startingValue = 0) => {
   let total = startingValue;
-  sourceArray.forEach(el => (total = callback(total,el)));
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (total === 0) {
+      total = sourceArray[0];
+      i = 1;
+    }
+
+    total = callback(total, sourceArray[i]);
+  }
   return total;
-}
+};
